93f65c09005af9a69c0599a6a45e86fd8272e961
GuilleEspinosa <guillermo.espinosa@sociuschile.cl>
2024-07-17 22:00:04 -0400

    pequeños diseños a homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,9 +14,12 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   double totalBalance = 0.0;
+  double totalExpenses = 0.0;
+  double totalIncome = 0.0;
   List<Map<String, dynamic>> recentTransactions = [];
   List<String> categories = ['Food', 'Transport', 'Entertainment'];
   bool isInitialBalanceSet = false;
+  int _selectedIndex = 0;
 
   void _addTransaction(String title, double amount, String type, String category) {
     setState(() {
@@ -30,19 +33,32 @@ class _HomeScreenState extends State<HomeScreen> {
 
       if (type == 'income') {
         totalBalance += amount;
+        totalIncome += amount;
       } else {
         totalBalance -= amount;
+        totalExpenses += amount;
       }
     });
   }
 
   void _updateTransaction(int index, Map<String, dynamic> updatedTransaction) {
     setState(() {
+      if (recentTransactions[index]['type'] == 'income') {
+        totalBalance -= recentTransactions[index]['amount'];
+        totalIncome -= recentTransactions[index]['amount'];
+      } else {
+        totalBalance += recentTransactions[index]['amount'];
+        totalExpenses -= recentTransactions[index]['amount'];
+      }
+
       recentTransactions[index] = updatedTransaction;
+
       if (updatedTransaction['type'] == 'income') {
         totalBalance += updatedTransaction['amount'];
+        totalIncome += updatedTransaction['amount'];
       } else {
         totalBalance -= updatedTransaction['amount'];
+        totalExpenses += updatedTransaction['amount'];
       }
     });
   }
@@ -51,8 +67,10 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       if (recentTransactions[index]['type'] == 'income') {
         totalBalance -= recentTransactions[index]['amount'];
+        totalIncome -= recentTransactions[index]['amount'];
       } else {
         totalBalance += recentTransactions[index]['amount'];
+        totalExpenses -= recentTransactions[index]['amount'];
       }
       recentTransactions.removeAt(index);
     });
@@ -78,134 +96,207 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    switch (index) {
+      case 0:
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => AddTransactionScreen(onAddTransaction: _addTransaction, categories: categories),
+          ),
+        );
+        break;
+      case 1:
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => CategoryScreen(
+              categories: categories,
+              onUpdateCategories: _updateCategories,
+              recentTransactions: recentTransactions,
+            ),
+          ),
+        );
+        break;
+      case 2:
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => CategoryChartScreen(recentTransactions: recentTransactions),
+          ),
+        );
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: _signOut,
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Total Balance: \$${totalBalance.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            if (!isInitialBalanceSet)
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => SetBalanceScreen(onSetBalance: _setInitialBalance),
-                    ),
-                  );
-                },
-                child: Text('Set Initial Balance'),
-              ),
-            SizedBox(height: 20),
-            Text(
-              'Recent Transactions',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              child: recentTransactions.isEmpty
-                  ? Center(child: Text('No recent transactions'))
-                  : Column(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            color: Color(0xFFefbdeb),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  AppBar(
+                    backgroundColor: Color(0xFFefbdeb),
+                    elevation: 0,
+                    title: Text('Home Screen'),
+                    actions: [
+                      IconButton(
+                        icon: Icon(Icons.logout),
+                        onPressed: _signOut,
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Total Balance: \$${totalBalance.toStringAsFixed(2)}',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        if (!isInitialBalanceSet)
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => SetBalanceScreen(onSetBalance: _setInitialBalance),
+                                ),
+                              );
+                            },
+                            child: Text('Set Initial Balance'),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Color(0xFFefbdeb),
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Expenses',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '\$${totalExpenses.toStringAsFixed(2)}',
+                      style: TextStyle(fontSize: 16, color: Colors.red),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Income',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '\$${totalIncome.toStringAsFixed(2)}',
+                      style: TextStyle(fontSize: 16, color: Colors.green),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Recent Transactions',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
                   Expanded(
-                    child: ListView.builder(
+                    child: recentTransactions.isEmpty
+                        ? Center(child: Text('No recent transactions'))
+                        : ListView.builder(
                       itemCount: recentTransactions.length < 4 ? recentTransactions.length : 4,
                       itemBuilder: (context, index) {
                         final transaction = recentTransactions[index];
-                        return ListTile(
-                          title: Text(transaction['title']),
-                          subtitle: Text('${transaction['date']} - ${transaction['category'] ?? ''}'),
-                          trailing: Text(
-                            '\$${transaction['amount'].toStringAsFixed(2)}',
-                            style: TextStyle(
+                        return Card(
+                          child: ListTile(
+                            title: Text(transaction['title']),
+                            subtitle: Text('${transaction['date']} - ${transaction['category'] ?? ''}'),
+                            trailing: Text(
+                              '\$${transaction['amount'].toStringAsFixed(2)}',
+                              style: TextStyle(
                                 color: transaction['type'] == 'income'
                                     ? Colors.green
-                                    : Colors.red),
+                                    : Colors.red,
+                              ),
+                            ),
                           ),
                         );
                       },
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => AllTransactionsScreen(
-                            transactions: recentTransactions,
-                            onUpdateTransaction: _updateTransaction,
-                            onDeleteTransaction: _deleteTransaction,
+                  if (recentTransactions.length > 4)
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => AllTransactionsScreen(
+                              transactions: recentTransactions,
+                              onUpdateTransaction: _updateTransaction,
+                              onDeleteTransaction: _deleteTransaction,
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                    child: Text('View All Transactions'),
-                  ),
+                        );
+                      },
+                      child: Text('View All Transactions'),
+                    ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => AddTransactionScreen(onAddTransaction: _addTransaction, categories: categories),
-                ),
-              );
-            },
-            child: Icon(Icons.add),
-          ),
-          SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => CategoryScreen(
-                    categories: categories,
-                    onUpdateCategories: _updateCategories,
-                    recentTransactions: recentTransactions,
-                  ),
-                ),
-              );
-            },
-            child: Icon(Icons.category),
-          ),
-          SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => CategoryChartScreen(recentTransactions: recentTransactions),
-                ),
-              );
-            },
-            child: Icon(Icons.pie_chart),
           ),
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add Transaction',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart),
+            label: 'Category Chart',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
+
+
+
+
+
 
 
 
